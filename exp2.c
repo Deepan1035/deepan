b6c436d54e87468a9f9b4ca5f0511eec8af932f4
Deepan1035 <deepan1035@gmail.com>
2025-02-12 14:15:15 +0530

    sub 5

---
+++ exp2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int number, result;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    result = number - 5;
+    printf("Result after subtracting 5: %d\n", result);
+
+    return 0;
+}
+
+
