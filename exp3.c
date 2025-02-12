100e4db1c0a1f961f4f758c6f11c6b998f8ade1e
Deepan1035 <deepan1035@gmail.com>
2025-02-12 14:22:26 +0530

    mulby3

---
+++ exp3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int number, result;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    result = number * 3;
+    printf("Result after multiplying by 3: %d\n", result);
+
+    return 0;
+}
+
