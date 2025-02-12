cd367133041ef350ebccc6abdaf196f33fbcf10d
Deepan1035 <deepan1035@gmail.com>
2025-02-12 14:25:56 +0530

    divby6

---
+++ exp4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>`
+int main() {
+    int number, quotient;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    quotient = number / 6;
+    printf("Quotient after dividing by 6: %d\n", quotient);
+    return 0;
+}
+
